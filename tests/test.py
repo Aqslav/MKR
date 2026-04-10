fb14d0cd5163a8da73b35ef236cc663d1657e06f
Aqslav <antonio.mudryj@gmail.com>
2026-04-10 15:26:48 +0300

    added stringify test

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+from IO_helper import IO_helper
+from population import Population
+import pytest
+
+@pytest.parametrize("data, expected", [
+    ([Population("CountryA", 2020, 1000000)], "CountryA,2020,1000000"),
+    ([Population("CountryB", 2019, 500000), Population("CountryC", 2021, 200000)], "CountryB,2019,500000\nCountryC,2021,200000"),
+    ([], "")
+])
+def test_stringify(data, expected):
+    assert IO_helper.stringify(data) == expected
+
